d87ee89b296d1f85c91700329cbfdb1e0916c33e
“Clement” <clemyjele@gmail.com>
2025-04-01 21:14:51 +0200

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Testimonies from "@/components/Services";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5" id="/">
       <div className="max-w-7xl w-full">
         
         <FloatingNavBar navItems={NavLinks}/>
