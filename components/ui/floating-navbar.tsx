d4f208b0e38d67fa37cc31180a15d9800ff67882
“Clement” <clemyjele@gmail.com>
2025-04-15 15:43:13 +0200

    floating bar fixed mx

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -94,13 +94,13 @@ export const FloatingNav = ({
     animate: { 
       y: 0, 
       opacity: 1, 
-      width: visible ? "100%" : "fit-content" 
+      width: visible ? "80%" : "fit-content" 
     },
     exit: { opacity: 1, y: 0 }
   };
 
   return (
-    <>
+    <div className="flex justify-center items-center">
       <motion.div
         initial="initial"
         animate="animate"
@@ -108,7 +108,7 @@ export const FloatingNav = ({
         variants={navVariants}
         transition={{ duration: 0.2 }}
         className={cn(
-          "flex fixed top-1 md:top-2 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] dark:bg-[#080a23] bg-gray-700 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[7000] px-8 py-2 items-center justify-between gap-1 rounded-md",
+          "flex fixed top-1 md:top-2 inset-x-0 mx-6 justify-center border border-transparent dark:border-white/[0.2] dark:bg-[#080a23] bg-gray-700 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[7000] px-8 py-2 items-center justify-between gap-1 rounded-md",
           className
         )}
       >
@@ -233,7 +233,7 @@ export const FloatingNav = ({
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
