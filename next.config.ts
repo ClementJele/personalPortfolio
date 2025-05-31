9cd5f7a23ad67d40caafc73fba89d28f3bcca388
Njabulo Jele <njabuloclement06@gmail.com>
2025-05-31 14:56:14 +0200

    updated staletimes for caching

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    staleTimes: {
+      static: 3600,
+      dynamic: 3600,
+    },
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
